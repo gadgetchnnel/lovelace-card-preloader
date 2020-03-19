fbebb4051853c19c9967e3c65b398e4675ba6228
Steven Rollason <2099542+gadgetchnnel@users.noreply.github.com>
2020-03-19 19:33:26 +0000

    Hide error messages when preloading cards

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 const helpers = require('custom-card-helpers');
+const console_error = window.console.error;
 
 const preloadCard = (type) => {
 	window.loadCardHelpers().then(({ createCardElement }) => {
+			window.console.error = function(){
+				if(arguments[0].includes(type)){
+					return;
+				}
+				
+				console_error(...arguments);
+			};
 			createCardElement({type});
+			window.console.error = console_error;
+			console.log(`Preloaded ${type}`);
 		})
 	}
 
@@ -12,7 +22,6 @@ function preloadCards(){
     let preload = helpers.getLovelace().config.preload;
     if(!preload) return;
     preload.forEach(cardtype => {
-    	console.log(`Preloading ${cardtype}`);
     	preloadCard(cardtype);	
     });
 }
